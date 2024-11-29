59608205d6857453b9d68f008780e6e40f68b733
drashtipatell18 <165013408+drashtipatell18@users.noreply.github.com>
2024-11-29 11:21:50 +0530

    Update Stock.php

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -8,6 +8,20 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Stock extends Model
 {
     use HasFactory, SoftDeletes;
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    public function subCategory()
+    {
+        return $this->belongsTo(SubCategory::class, 'sub_category_id');
+    }
     protected $fillable = [
         'category_id',
         'sub_category_id',
